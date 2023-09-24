7985fb1a2394fd1d63ce5b8a3a53dfcd634865da
JSpapi <testing@gmail.com>
2023-09-25 01:18:37 +0500

    feat: init rtk query api service

--- src/store.tsx
+++ src/store.tsx
@@ -1,5 +1,14 @@
-import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { api } from './services/api';
 
-export const store = configureStore({});
+const reducers = combineReducers({
+  [api.reducerPath]: api.reducer,
+});
+
+export const store = configureStore({
+  reducer: reducers,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
+});
 
 export type RootState = ReturnType<typeof store.getState>;
