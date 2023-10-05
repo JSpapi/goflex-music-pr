42320386bdca49d88227851563c4dc188ca1c1f7
JSpapi <testing@gmail.com>
2023-10-05 05:29:42 +0500

    feat: add verify OTP code request

--- src/authPages/resetPassword/ConfirmOtpCode.tsx
+++ src/authPages/resetPassword/ConfirmOtpCode.tsx
@@ -1,18 +1,47 @@
 import { Container } from '@mui/material';
-import React, { ChangeEvent, useEffect, useRef, useState } from 'react';
+import {
+  ChangeEvent,
+  FormEvent,
+  Fragment,
+  KeyboardEvent,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
+import {
+  createSearchParams,
+  useNavigate,
+  useSearchParams,
+} from 'react-router-dom';
+import { toast } from 'react-toastify';
 import { AuthTitle } from '../../components/authTitle/AuthTitle';
+import { useVerifyOTPQuery } from '../../services/auth.api';
 import s from '../authPages.module.scss';
 
 let currentOTPIndex = 0;
 export function ConfirmOtpCode() {
+  // todo GETTING PARAMS
+  const [searchParams] = useSearchParams();
+  const navigate = useNavigate();
+
   const [otp, setOtp] = useState<string[]>(new Array(6).fill(''));
   const [activeOTPIndex, setActiveOTPIndex] = useState(0);
+  const [name, setName] = useState(searchParams.get('name') || '');
+  const [skipVerifyRequest, setSkipVerifyRequest] = useState(true);
 
+  // TODO 5 VERIFY OTP REQUEST
+  const {
+    isError: verifyError,
+    isSuccess: verifySuccess,
+    isLoading: verifyLoading,
+  } = useVerifyOTPQuery(
+    { code: otp.join(''), name },
+    { skip: skipVerifyRequest }
+  );
   const inputRef = useRef<HTMLInputElement>(null);
 
   // TODO 1 GET VALUE OR DELETE VALUE OF EACH INPUT AND SET TO OTP STATE
-
-  const handleOtpChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
+  const onHandleOtpChange = ({ target }: ChangeEvent<HTMLInputElement>) => {
     const { value } = target;
     const newOtp: string[] = [...otp];
     newOtp[currentOTPIndex] = value.substring(value.length - 1);
@@ -22,9 +51,10 @@ export function ConfirmOtpCode() {
 
     setOtp(newOtp);
   };
+
   // TODO 2 DELETE VALUE ON BACKSPACE KEYDOWN
   const onHandleKeyDown = (
-    { key }: React.KeyboardEvent<HTMLInputElement>,
+    { key }: KeyboardEvent<HTMLInputElement>,
     index: number
   ) => {
     currentOTPIndex = index;
@@ -35,6 +65,45 @@ export function ConfirmOtpCode() {
     inputRef.current?.focus();
   }, [activeOTPIndex]);
 
+  // TODO 4 SUBMIT FORM AND ACTIVATE VERIFY OTP GET REQUEST
+  const onVerifyOtpSubmit = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSkipVerifyRequest(false);
+  };
+
+  // TODO 5 CHECK VERIFY REQUEST SUCCESS OR ERROR
+  useEffect(() => {
+    if (verifyError) {
+      toast.error('Invalid OTP', {
+        position: 'top-right',
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'dark',
+      });
+      setSkipVerifyRequest(true);
+    } else if (verifySuccess) {
+      toast.success(`Welcome back ${name} ^_^`, {
+        position: 'top-right',
+        autoClose: 3000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: 'dark',
+      });
+      navigate({
+        pathname: '/resetPassword',
+        search: createSearchParams({ name }).toString(),
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [verifyError, verifySuccess]);
+
   return (
     <div className={s.authorization}>
       <Container maxWidth="xl">
@@ -44,20 +113,20 @@ export function ConfirmOtpCode() {
           verify
         </AuthTitle>
         <div className={s.authorization_fields} style={{ paddingTop: '100px' }}>
-          <form className={s.authorization_form}>
+          <form className={s.authorization_form} onSubmit={onVerifyOtpSubmit}>
             <div className={s.authorization_form__otp}>
               {otp.map((_, index) => (
                 // eslint-disable-next-line react/no-array-index-key
-                <React.Fragment key={index}>
+                <Fragment key={index}>
                   <input
                     ref={index === activeOTPIndex ? inputRef : null}
                     type="number"
                     className={s.otpField}
                     onKeyDown={(e) => onHandleKeyDown(e, index)}
-                    onChange={handleOtpChange}
+                    onChange={onHandleOtpChange}
                     value={otp[index]}
                   />
-                </React.Fragment>
+                </Fragment>
               ))}
             </div>
 
@@ -68,7 +137,12 @@ export function ConfirmOtpCode() {
               </button>
             </p>
 
-            <button className={s.authorization_form__btn} type="submit">
+            <button
+              className={s.authorization_form__btn}
+              type="submit"
+              style={verifyLoading ? { opacity: 0.5 } : { opacity: 1 }}
+              disabled={verifyLoading}
+            >
               Verify code
             </button>
           </form>
