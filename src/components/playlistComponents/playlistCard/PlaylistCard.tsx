ad7a28df0abd6dcbeec3d866061cc352b6f63800
JSpapi <testing@gmail.com>
2023-10-31 21:07:53 +0500

    feat: add playlist circle component

--- src/components/playlistComponents/playlistCard/PlaylistCard.tsx
+++ src/components/playlistComponents/playlistCard/PlaylistCard.tsx
@@ -4,14 +4,14 @@ import s from './PlaylistCard.module.scss';
 
 export function PlaylistCard() {
   return (
-    <NavLink to="/" className={s.playlist}>
-      <div className={s.playlist_img}>
+    <NavLink to="/" className={s.playlist_card}>
+      <div className={s.playlist_card_img}>
         <img
           src="https://res.cloudinary.com/dx3tdf3e7/image/upload/v1698669050/auditica-files/y1lwpagnyoxofhwxzmd4.jpg"
           alt="test"
         />
       </div>
-      <div className={s.playlist_info}>
+      <div className={s.playlist_card_info}>
         <h4 className="font_regular">Red (Taylor&apos;s Version)</h4>
         <NavLink to="/test" className="font_subregular">
           Taylor Swift
